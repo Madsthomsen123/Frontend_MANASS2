a26c2be466f1f76edddedec36ed93d3fd790e1ae
Mathias Madsen <202010725@post.au.dk>
2023-05-03 14:40:53 +0200

    der kan fandme logges ind som model og se næsten ingen ting

--- modelmanagement/src/components/Navbar.jsx
+++ modelmanagement/src/components/Navbar.jsx
@@ -2,7 +2,31 @@ import { NavLink } from "react-router-dom";
 import "./css/Navbar.css"
 
 
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
+
+
+
+
 export function Navbar() {
+
+    const token = localStorage.getItem("token");
+
+    let user = parseJwt(token);
+    let isManager = user["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]==="Manager";
+    console.log(user);
+    console.log(isManager);
+
+
+if(isManager){
 return (
     <nav className="nav_link">
     <NavLink to="/">
@@ -28,5 +52,17 @@ return (
     </NavLink>
 
 </nav>
+)}
+else
+return(
+    <nav className="nav_link">
+    <NavLink to="/">
+        Login    
+    </NavLink>
+    <NavLink to="/jobList">
+    Job List   
+    </NavLink>
+    </nav>
+
 )
 }
